65e9abe8e80df1ba48a3523b21ea236092d17dc5
rickherrera3001 <rihr.3001@gmail.com>
2024-04-03 15:44:07 -0600

    Initial Commit

---
+++ HackerNewsRM/HackerNewsRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsRMApp.swift
+//  HackerNewsRM
+//
+//  Created by Ricardo Developer on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
